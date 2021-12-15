e66624b80f099bc5f6ff48c9522d09a5e7da9f87
Burlington 1998 <96153968+Burlington1998@users.noreply.github.com>
2021-12-15 03:06:11 +0000

    Log in and sign up forms GUI completed

---
+++ public/client/action.js
@@ -0,0 +1,9 @@
+$("#alt-log-on").click(function () {
+  $("#signup-form").hide();
+  $("#login-form").show();
+});
+
+$("#alt-sign-up").click(function () {
+  $("#login-form").hide();
+  $("#signup-form").show();
+});
