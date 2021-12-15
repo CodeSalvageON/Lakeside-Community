ba4ec47caf627e0fb6a413b194b461f82ec0561d
Burlington 1998 <96153968+Burlington1998@users.noreply.github.com>
2021-12-15 05:30:55 +0000

    Finished

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const express = require('express');
+const sanitizer = require('sanitizer');
+
+const key = process.env.KEY;
+const encryptor = require('simple-encryptor')(key);
 
 var app = require('express')();
 var http = require('http').Server(app);
@@ -7,15 +11,61 @@ var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 var io = require('socket.io')(http);
+var atob = require('atob');
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let chatLog = "";
+
+const cipher = salt => {
+    const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+    const byteHex = n => ("0" + Number(n).toString(16)).substr(-2);
+    const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
+
+    return text.split('')
+      .map(textToChars)
+      .map(applySaltToChar)
+      .map(byteHex)
+      .join('');
+}
+    
+const decipher = salt => {
+    const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+    const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
+    return encoded.match(/.{1,2}/g)
+      .map(hex => parseInt(hex, 16))
+      .map(applySaltToChar)
+      .map(charCode => String.fromCharCode(charCode))
+      .join('');
+}
+
 app.get('', function (req, res) {
   const index = __dirname + '/public/client/index.html';
 
   res.sendFile(index);
+});
+
+global.atob = function (b64Encoded) {
+  return new Buffer(b64Encoded, 'base64').toString('binary');
+};
+
+app.post('/chat', function (req, res) {
+  const username = req.body.username;
+  const msg = req.body.msg;
+
+  let w_u = atob(username);
+  let w_m = atob(msg);
+
+  const d_username = sanitizer.escape(w_u);
+  const d_msg = sanitizer.escape(w_m);
+
+  chatLog += "<p><p class='blue'>" + d_username + "</p> " + d_msg + "</p>";
+});
+
+app.get('/chat', function (req, res) {
+  res.send(chatLog);
 });
 
 http.listen(port, function(){
--- public/client/action.js
+++ public/client/action.js
@@ -1,3 +1,34 @@
+function makeid (length) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+let coolID = "";
+let encryptionKey = "";
+let username = "";
+
+let localEncryptionKey = localStorage.getItem("bhsim_key");
+let localUsername = localStorage.getItem("bhsim_username");
+
+const signupUsername = document.getElementById("screen-name-signup");
+const signupPassword = document.getElementById("password-signup");
+
+const loginUsername = document.getElementById("screen-name-login");
+const loginPassword = document.getElementById("password-login");
+
+const loginMSG = document.getElementById("login-msg");
+const msgWriteValue = document.getElementById("msg-text");
+const chatbox = document.getElementById("chatbox");
+
+let oldChat = "";
+
 $("#alt-log-on").click(function () {
   $("#signup-form").hide();
   $("#login-form").show();
@@ -7,3 +38,74 @@ $("#alt-sign-up").click(function () {
   $("#login-form").hide();
   $("#signup-form").show();
 });
+
+$("#signup-form").submit(function () {
+  event.preventDefault();
+  
+  coolID = makeid(7);
+  encryptionKey = signupPassword.value;
+  localStorage.setItem("bhsim_key", encryptionKey);
+  username = signupUsername.value;
+  localStorage.setItem("bhsim_username", username);
+
+  $("#startup").hide();
+  $("#chat").show();
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+});
+
+$("#login-form").submit(function () {
+  event.preventDefault();
+  
+  if (loginPassword.value === localEncryptionKey) {
+    $("#startup").hide();
+    $("#chat").show();
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+  }
+
+  else {
+    loginMSG.innerText = "Error: Incorrect Encryption Key.";
+  }
+});
+
+$("#msg-write").submit(function () {
+  event.preventDefault();
+
+  fetch ("/chat", {
+    method : "POST",
+    headers : {
+      'Content-Type' : 'application/json'
+    },
+    body : JSON.stringify({
+      username : btoa(localUsername),
+      msg : btoa(msgWriteValue.value)
+    })
+  })
+  .then(response => response.text())
+  .then(data => {
+    console.log(data); 
+    msgWriteValue.value = "";
+  })
+  .catch(error => {
+    throw error;
+  });
+});
+
+setInterval(function () {
+  fetch ("/chat")
+  .then(response => response.text())
+  .then(data => {
+    if (data === oldChat) {
+      // do nothing
+    }
+
+    else {
+      chatbox.innerHTML = data;
+      oldChat = data;
+
+      chatbox.scrollTo(0, chatbox.scrollHeight);
+    }
+  })
+  .catch(error => {
+    throw error;
+  });
+}, 500);
